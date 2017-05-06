dccfba84f5a05388c137c6f6daeaca0cd6598c0f
kalia22 <kalialix21@gmail.com>
2017-05-06 23:12:28 +0200

    Update entero.c

--- entero.c
+++ entero.c
@@ -2,28 +2,32 @@
 #include <stdlib.h>
 #include "vector.h"
 
+typedef int TELEMENTO;
 
 typedef struct {
-    TELEMENTO *cifras;
+    vectorP *cifras;
     int signo;
     unsigned long int tam;
 }STENTERO;
 typedef STENTERO *entero; 
 
-
-
-void creaEnteroLargo(entero *v1, unsigned long int tam1, char signo){
+void creaEnteroLargo(entero *v1, unsigned long int tam1, int signo){
 
     unsigned long int i=0;
     *v1=(entero)malloc(sizeof(STENTERO));
-    (*v1)->cifras=(TELEMENTO*)malloc(tam1*sizeof(TELEMENTO)); //TELEMENTO por entero
+    (*v1)->cifras=(TELEMENTO*)malloc(tam1*sizeof(TELEMENTO)); 
+    
     (*v1)->tam=tam1;
     for(i=0; i < tam1; i++){
-        *((*v1)->cifras+i) = 0;
+        *((*v1)->cifras+i)=0;
+           
 }
+    
+}
+
 void asignarEntero(entero *v1, unsigned long int posicion, TELEMENTO valor)
 {
-    *((*v1)->cifras+posicion)=valor;
+    *((*v1)->cifras + posicion) = valor;
 }
 
 void liberarEntero(entero *v1){
@@ -32,19 +36,12 @@ void liberarEntero(entero *v1){
 
 }
 
-TELEMENTO recuperarEntero(entero v1, unsigned long int posicion){
-
-    if (posicion >= 0 && posicion < v1->tam)
-    {
-        return v1->cifras[posicion];
-    }
-    return 0;
-
+TELEMENTO recuperarEntero(entero *v1, unsigned long int posicion){
+    
+        return *((*v1)->cifras+posicion);
 }
 
 unsigned long int tamanhoEnt(entero v1){
 
     return v1->tam;
-}
-
 }
