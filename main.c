9347a05ae5b3d58191448ca56eb431fbc5acca15
kalia22 <kalialix21@gmail.com>
2017-05-06 23:16:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "entero.h"
-#include "vector.h"
+#include "enteroLargo.h"
+#include "vectordinamico.h"
 
+void multclas(entero v1, entero v2, entero *resultado){
+    
+    int i,j;
+    unsigned long int tm = tamanhoEnt(v1);
+    int k = 2*tm-1;
+
+    for(i=tm-1; i>=0; i--){
+
+       for(j=tm-1; j>=0; j--){
+           
+            asignarEntero(resultado, k, (recuperarEntero(resultado, k)+recuperarEntero(&v1, j)*recuperarEntero(&v2, i)));
+	    
+printf("%d\n",recuperarEntero(&v1, j));
+printf("%d\n",recuperarEntero(&v2, j));
+            k--;
+       }
+
+       k = 2*tm-1;
+    }
+
+
+    for(i=k; i>=0; i--){
+
+        if(recuperarEntero(resultado,i)>=10){
+            
+                asignarEntero(resultado, i-1, (recuperarEntero(resultado, i-1)+(recuperarEntero(resultado, i)-recuperarEntero(resultado, i)%10)/10));
+                asignarEntero(resultado,i, recuperarEntero(resultado, i)%10 );
+                
+        }
+    
+    }
+
+
+//imprimir
+    for(i=0; i<2*tm; i++){
+        printf("%d", recuperarEntero(resultado, i));
+    }
+
+}
 
 int main(int argc, char** argv) {
+
     entero v1=NULL;
     entero v2=NULL;
     entero result=NULL;
     int flag1, flag2=0;
-    int i, j=0;
-    unsigned long int numcif;
-
+    
     if(argc>1){
+        
+//comprobar signo
+        int numcif=strlen(argv[1]);
 
-        numcif=strlen(argv[1]);
+
 
         if(argv[1][0]=='-'){
             numcif-1;
@@ -25,10 +66,13 @@ int main(int argc, char** argv) {
         if(argv[2][0]=='-'){
             flag2=1;
         }
+
         
         creaEnteroLargo(&v1, numcif, flag1);
         creaEnteroLargo(&v2, numcif, flag2);
 
+
+        //comprobacion del signo del resultado
         if((flag1==1 && flag2==0) || (flag2==1 && flag1==0)){
              creaEnteroLargo(&result, numcif*2, 1);
         }
@@ -36,8 +80,10 @@ int main(int argc, char** argv) {
             creaEnteroLargo(&result, numcif*2, 0);
         }
 
+
         //asignar valores
-      for(int i=0; argv[1][i] !="/0" ;i++){
+	
+      for(int i=0; argv[1][i] != '\0';i++){
             
             if(flag1==1){
                 i++;
@@ -45,10 +91,11 @@ int main(int argc, char** argv) {
 
             int cifra = (int)argv[1][i]-'0';
             asignarEntero(&v1, i, cifra);
-
+            
         }
+
         
-        for(int i=0; argv[2][i] !="/0";i++){
+        for(int i=0; argv[1][i] != '\0';i++){
             if(flag2==1){
                 i++;
             }
@@ -57,7 +104,9 @@ int main(int argc, char** argv) {
 
         }
 
+       multclas(v1, v2, &result);
     }
 
     return (EXIT_SUCCESS);
 }
+
